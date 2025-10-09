53b51ca21500c86dd6d1800702d0db1654def411
Sall62-bit <maxencesveic62@gmail.com>
2025-10-09 11:56:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('SapaButton')
+const tombolSapa = document.getElementById('sapaButton')
 tombolSapa.addEventListener('click', function() {
   alert('Kon gabut a kok metek iki');
 });
